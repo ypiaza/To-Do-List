4f588c8fc71c878be2743d9daa5476c9a0ebfffb
Yuri Piaza <yuripiazagomessantos@gmail.com>
2024-10-22 13:24:31 -0300

    Inclusao do botão de concluir tarefa e inicio de span de mensagem de confirmação (de exclusao e conclusão)

--- script.js
+++ script.js
@@ -3,39 +3,31 @@
 const btnAddTarefa = document.getElementById('btn-add-tarefa');
 const inputTarefa = document.getElementById('tarefa');
 const listaTarefas = document.getElementById('listaTarefas');
+const areaTasks = document.getElementById('body')
 
 function addTarefa(event) {
     const tarefaEscrita = inputTarefa.value;
 
     if (tarefaEscrita) {
         const tarefa = document.createElement('li');
+        const btnArea = document.createElement('div')
         const btnDelete = document.createElement('button');
+        const btnCheck = document.createElement('button');
         btnDelete.innerHTML = '<i class="fa-solid fa-trash"></i>';
+        btnCheck.innerHTML = '<i class="fa-solid fa-check"></i>'
         tarefa.innerText = tarefaEscrita;
+        btnArea.classList.add('btnArea')
         listaTarefas.appendChild(tarefa);
-        tarefa.appendChild(btnDelete);
+        tarefa.appendChild(btnArea);
+        btnArea.appendChild(btnDelete);
+        btnArea.appendChild(btnCheck);
         tarefaEscrita.value = '';
 
 
-        btnDelete.addEventListener('mouseover', (event) => {
-            if (event.currentTarget === btnDelete) {
-                tarefa.style.backgroundColor = 'var(--bg-task-delete-red)'
-                btnDelete.style.backgroundColor = 'transparent'
-                btnDelete.style.rotate = '10deg'
-            }
-
-        });
-
-        btnDelete.addEventListener('mouseout', (event) => {
-            if (event.currentTarget !== tarefa) {
-                tarefa.style.backgroundColor = ''
-                btnDelete.style.backgroundColor = ''
-                btnDelete.style.rotate = ''
-            }
-        })
-
         btnDelete.addEventListener('click', () => {
             tarefa.remove(tarefa);
+            const alertDel = document.createElement('div')
+            
         })
 
         tarefa.addEventListener('click', (event) => {
